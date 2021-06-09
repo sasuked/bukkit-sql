325a6194352dbc6cea3522f4852b229064ab8f60
sasuked <majinboo.email@gmail.com>
2021-06-09 05:38:47 -0300

    Fixed MysqlConnectionProvider error.

--- src/main/java/io/github/sasuked/bukkitsql/provider/mysql/MysqlConnectionProvider.java
+++ src/main/java/io/github/sasuked/bukkitsql/provider/mysql/MysqlConnectionProvider.java
@@ -7,17 +7,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public final class MysqlConnectionProvider extends ConnectionProvider {
-	
+
 	private static final String MYSQL_CONNECTION_STRING = "jdbc:mysql://<host>:<port>/<database>";
-	
-	private String host;
-	private int port;
-	private String databaseName;
-	private String user;
-	private String password;
-	
+
+	private final String host;
+	private final int port;
+	private final String databaseName;
+	private final String user;
+	private final String password;
+
 	private Connection cachedConnection;
-	
+
 	public MysqlConnectionProvider(String host, int port, String databaseName, String user, String password) {
 		this.host = host;
 		this.port = port;
@@ -25,38 +25,38 @@ public final class MysqlConnectionProvider extends ConnectionProvider {
 		this.user = user;
 		this.password = password;
 	}
-	
+
 	@SneakyThrows
 	@Override
 	public Connection getCurrentConnection() {
 		if (cachedConnection != null && !cachedConnection.isClosed()) {
 			return cachedConnection;
 		}
-		
+
 		cachedConnection = this.connect();
 		if (cachedConnection != null) {
 			return cachedConnection;
 		}
-		
+
 		throw new NullPointerException("Conexão foi nula, verifique as credenciais!");
 	}
-	
+
 	private Connection connect() {
 		try {
 			// pqp
 			Class.forName("com.mysql.jdbc.Driver");
-			
-			return DriverManager.getConnection(MYSQL_CONNECTION_STRING, user, password);
+
+			return DriverManager.getConnection(getConnectionString(), user, password);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
-	
+
+
 	private String getConnectionString() {
 		return MYSQL_CONNECTION_STRING.replace("<host>", host)
 			.replace("<port>", String.valueOf(port))
-			.replace("<database>", password);
+			.replace("<database>", databaseName);
 	}
 }
